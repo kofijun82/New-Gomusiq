5d77af185b088741acf5e595d7a60acb4539ac68
ackalfred <ackjunioralfred@gmail.com>
2024-12-30 10:58:17 +0000

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import ChatPage from "./pages/chat/ChatPage";
+import AlbumPage from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+
+import { Toaster } from "react-hot-toast";
+import NotFoundPage from "./pages/404/NotFoundPage";
+import { Route, Routes } from "react-router-dom";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/home/HomePage";
+
+
+function App() {
+  
+  return (
+		<>
+			<Routes>
+				<Route
+					path='/sso-callback'
+					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
+				/>
+				<Route path='/auth-callback' element={<AuthCallbackPage />} />
+				<Route path='/admin' element={<AdminPage />} />
+
+				<Route element={<MainLayout />}>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} />
+				</Route>
+			</Routes>
+			<Toaster />
+		</>
+	);
+}
+
+export default App
